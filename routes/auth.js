bfe7dcb8ccc2d28d4ba5e58b965c7e37e5498d97
Niennis <estefania.osses.v@gmail.com>
2021-02-11 09:01:33 -0300

    Auth validation and creating token

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const config = require('../config');
+const User = require('../models/User');
 
 const { secret } = config;
 
@@ -17,7 +19,8 @@ module.exports = (app, nextMain) => {
    * @code {400} si no se proveen `email` o `password` o ninguno de los dos
    * @auth No requiere autenticación
    */
-  app.post('/auth', (req, resp, next) => {
+  app.post('/auth', async (req, resp, next) => {
+
     const { email, password } = req.body;
 
     if (!email || !password) {
@@ -25,7 +28,35 @@ module.exports = (app, nextMain) => {
     }
 
     // TODO: autenticar a la usuarix
-    next();
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+      return next(404)
+    }
+
+    const matches = await bcrypt.compare(password, user.password);
+    if (!matches) {
+      // si no coincide, no autorizado
+      return next(404)
+    }
+
+    // en mongo se crea una _id automáticamente
+    return resp.json({
+      token: jwt.sign({ uid: user._id }, secret),
+    });
+    
+    /*if (email === adminEmail && password === adminPassword) {
+      // var token = jwt.sign({ foo: 'bar' }, secret);
+      // sign es para firmar, ¿qué se firma? al usuario
+      // secret es la llave de encriptación
+      // console.log(token);
+      return resp.json({
+        token: jwt.sign({ uid: 'admin' }, secret),
+      });
+    }*/
+
+    // resp.json({ email, password })
+    // next();
+    // return next(401);
   });
 
   return nextMain();
