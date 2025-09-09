006129bf5876dd2582e0e8d0c6f3933f6744277b
Estefania <estefania.osses.v@gmail.com>
2025-09-09 13:46:33 -0300

    Config cors

--- index.js
+++ index.js
@@ -5,14 +5,28 @@ const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
 const routes = require('./routes');
 const pkg = require('./package.json');
-const cors=require('cors')
+const cors = require('cors')
 
 const { port, dbUrl, secret } = config;
 const app = express();
 
+const allowedOrigins = [
+  "https://bakequeen.vercel.app", // frontend en Vercel
+  "http://localhost:3000"         // para desarrollo local
+];
+
 const corsOptions = {
-  exposedHeaders: 'Link'
-}
+  origin: function (origin, callback) {
+    // permitir también requests sin "origin" (ej: curl, Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  exposedHeaders: ["Link"],
+  credentials: true, // solo para cookies/session
+};
 
 // TODO: Conexión a la Base de Datos (MongoDB o MySQL)
 console.log('dburl', process.env.DB_URL)
